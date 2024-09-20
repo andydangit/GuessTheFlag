50a2a1f072e29ff8e667650a937dc1ce9a3f2494
andydangit <andy.dang.it@gmail.com>
2024-09-20 15:19:27 -0500

    Add: LinearGradient  use  .init instead of gradient.stop

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,18 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
-            }
-            Text("Your Content")
-                .foregroundStyle(.secondary)
-                .padding(60)
-                .background(.ultraThinMaterial)
-        }
-                .ignoresSafeArea()
-        
+        LinearGradient(stops:[
+            .init(color: .white, location: 0.45),
+            .init(color: .black, location: 0.55)
+        ], startPoint: .top, endPoint: .bottom)
+     
+            .ignoresSafeArea()
     }
 }
 
