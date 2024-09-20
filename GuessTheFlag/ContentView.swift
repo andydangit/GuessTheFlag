2513fc1c52225e7d22a84ce3099581a5510805a2
andydangit <andy.dang.it@gmail.com>
2024-09-20 16:57:37 -0500

    Add: Button style

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,20 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        RadialGradient(colors: [.purple, .black], center: .center, startRadius: 20, endRadius: 200)
+        //        RadialGradient(colors: [.purple, .black], center: .center, startRadius: 20, endRadius: 200)
         
-//        AngularGradient(colors: [.red, .green, .blue, .purple, .red], center: .center)
-     
-        Text("Your content")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .foregroundStyle(.white)
-            .background(.red.gradient)
+        //        AngularGradient(colors: [.red, .green, .blue, .purple, .red], center: .center)
         
-
+        //        Text("Your content")
+        //            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        //            .foregroundStyle(.white)
+        //            .background(.red.gradient)
+        //
+        
+        Button {
+            print("Button was tapped")
+        }label: {
+            Label("Edit", systemImage: "pencil")
+                .padding()
+                .foregroundColor(.white)
+                .background(.gray)
+        }
+            
+  
+        
     }
+    func executDelete() {
+        print("Now Deleting")
+    }
+    
 }
 
 #Preview {
     ContentView()
 }
- 
+
