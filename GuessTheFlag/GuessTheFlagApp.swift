4cb79a67fc0ec72df3999b4c28aded60601f1f94
andydangit <andy.dang.it@gmail.com>
2024-09-20 14:21:34 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Andy Dang It on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
