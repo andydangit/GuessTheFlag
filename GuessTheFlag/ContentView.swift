7f7eaffcd05b391a99aa6e31eccb1f88fe831001
andydangit <andy.dang.it@gmail.com>
2024-09-21 06:54:04 -0500

    Add: Flag asset and remove the old content

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,34 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
     
     var body: some View {
-        //        RadialGradient(colors: [.purple, .black], center: .center, startRadius: 20, endRadius: 200)
-        
-        //        AngularGradient(colors: [.red, .green, .blue, .purple, .red], center: .center)
-        
-        //        Text("Your content")
-        //            .frame(maxWidth: .infinity, maxHeight: .infinity)
-        //            .foregroundStyle(.white)
-        //            .background(.red.gradient)
-        //
-        
-        Button("Show Alert", role: .destructive) {
-            showingAlert = true
-                
-        }
-        
-        .padding()
-        .alert("Important message!", isPresented: $showingAlert) {
-            Button("Delete", role: .destructive) {}
-            Button("Cancel", role: .cancel) {}
-        } message: {
-        Text("Please read this.")
+        VStack {
+            
         }
           
     }
-
     
 }
 
